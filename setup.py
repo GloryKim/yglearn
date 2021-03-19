dd60d88536ae2fb967186eef2edc38277db37de9
yklovejesit config --global user.email yklovejesus@gmail.com <yklovejesus@gmail.com>
2021-03-20 00:01:41 +0900

    Update:setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name                = 'glory_lib',
-    version             = '0.1',
+    version             = '0.2',
     description         = 'pip install test',
     author              = 'GloryKim',
     author_email        = 'yklovejesus@gmail.com',
