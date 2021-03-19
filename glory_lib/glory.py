d02da5aa0ef1cd7baef7e5169511a5870844cfa4
yklovejesit config --global user.email yklovejesus@gmail.com <yklovejesus@gmail.com>
2021-03-19 23:11:06 +0900

    Create:yglearn

---
+++ glory_lib/glory.py
@@ -0,0 +1,2 @@
+def print_soojin(msg):
+    print(f"라이브러리 {msg}")
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name                = 'glory_lib',
+    version             = '0.1',
+    description         = 'pip install test',
+    author              = 'GloryKim',
+    author_email        = 'yklovejesus@gmail.com',
+    url                 = 'https://github.com/GloryKim/yglearn',
+    download_url        = 'https://github.com/GloryKim/yglearn/archive/0.0.tar.gz',
+    install_requires    =  [],
+    packages            =  ['glory_lib'],
+    keywords            = ['ccpy'],
+    python_requires     = '>=3',
+    package_data        = {},
+    zip_safe            = False,
+    classifiers         = [
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
